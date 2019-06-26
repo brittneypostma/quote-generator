662d357a1594a990ad75c5ce3d18a6df3df57968
Brittney Postma <brittney.postma@hotmail.com>
2019-06-26 13:27:40 -0400

    Updates

---
+++ precache-manifest.40c90cd8ff30583bbda43022ff4e26b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf22c4bd6448e69afa671635cd01d3bd",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "321757905e6b5ff41abe",
+    "url": "/quote-generator/static/css/main.19e49b08.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/quote-generator/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "321757905e6b5ff41abe",
+    "url": "/quote-generator/static/js/main.a768ce1a.chunk.js"
+  },
+  {
+    "revision": "b42996cbb3818e11c29d",
+    "url": "/quote-generator/static/js/runtime~main.4226e67e.js"
+  }
+]);
